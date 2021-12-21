6d587a8bbe74c46aee1ae5d22a0075a73b6f3836
djvruio <djvruio@gmail.com>
2021-12-20 19:29:39 -0500

    Trigger lambda function from S3 bucket using blueprint.

---
+++ s3-get-object-python-blueprint/lambda_function.py
@@ -0,0 +1,30 @@
+import json
+import urllib.parse
+import boto3
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+
+    # Get the object from the event and show its content type
+    # NOTE: The bucket must be in the same region as the function
+    
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+
+    print(f'bucket: {bucket}')
+    print(f'key: {key}')
+    print(f'context.log_group_name: {context.log_group_name}')
+    
+    try:
+        response = s3.get_object(Bucket=bucket, Key=key)
+        print("CONTENT TYPE: " + response['ContentType'])
+        return response['ContentType']
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
